326d2d0c522cd083e663575633e5cb9dac4c096a
Ece Naz Genç <gencece123@gmail.com>
2025-12-25 20:53:13 +0300

    Refactor import statements and __all__ declaration

--- backend/controllers/__init__.py
+++ backend/controllers/__init__.py
@@ -1,25 +1,12 @@
-"""
-Controller Layer - API Endpoints
-"""
+"""CONTROLLERS - API Endpoint'leri"""
+from controllers.auth_controller import auth_bp
+from controllers.user_controller import user_bp
+from controllers.author_controller import author_bp
+from controllers.category_controller import category_bp
+from controllers.book_controller import book_bp
+from controllers.transaction_controller import transaction_bp
+from controllers.penalty_controller import penalty_bp
+from controllers.member_controller import member_bp
+from controllers.stats_controller import stats_bp
 
-from .auth_controller import auth_bp
-from .user_controller import user_bp
-from .author_controller import author_bp
-from .category_controller import category_bp
-from .book_controller import book_bp
-from .transaction_controller import transaction_bp
-from .penalty_controller import penalty_bp
-from .member_controller import member_bp
-from .stats_controller import stats_bp
-
-__all__ = [
-    'auth_bp',
-    'user_bp',
-    'author_bp',
-    'category_bp',
-    'book_bp',
-    'transaction_bp',
-    'penalty_bp',
-    'member_bp',
-    'stats_bp'
-]
+__all__ = ['auth_bp', 'user_bp', 'author_bp', 'category_bp', 'book_bp', 'transaction_bp', 'penalty_bp', 'member_bp', 'stats_bp']
